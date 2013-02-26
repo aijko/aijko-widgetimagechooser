32c2f0fd99dcf5deb38e66006746b3f7c40cbc5d
Gerrit Pechmann <gp@aijko.de>
2013-02-26 22:41:51 +0100

    Add class comments and CGL violations.

--- app/code/community/Aijko/WidgetImageChooser/Helper/Data.php
+++ app/code/community/Aijko/WidgetImageChooser/Helper/Data.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * Image chooser data helper.
+ *
+ * @category    Aijko
+ * @package     Aijko_WidgetImageChooser
+ * @author      Alexander Menk
+ * @copyright   Copyright (c) 2012 aijko GmbH (http://www.aijko.de)
+ */
 class Aijko_WidgetImageChooser_Helper_Data extends Mage_Core_Helper_Abstract
 {
 
